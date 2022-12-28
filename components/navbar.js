d6717666458a5c1b63819d65a902281fe1374232
Brandon Velarde <bvelarde@Brandons-MBP.attlocal.net>
2022-12-28 10:17:58 -0600

    added breakpints, added custom color, working through nav. Creating New smaller hero image.

--- components/navbar.js
+++ components/navbar.js
@@ -2,54 +2,60 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import navLogo from '../assets/homebuttonwht.png';
-import insta from '../assets/instaicon.png';
-import faceb from '../assets/fbicon.png';
+import insta from '../assets/Asset3.png';
+import faceb from '../assets/Asset1.png';
 
 
 export default function Navbar(){
     return(
-        <nav>
+        <nav className='bg-black flex'>
 
-        <div className='bg-black flex flex-row-reverse '>
-
-        <div  >
-        <button className='text-white'>BOOK CONSULT</button>
-        </div>
-
-        <div>
-        <button className='text-white'>
+        <div className=''>
+        <a href='#' className='flex justify-center'>
         <Image 
-        src={insta} 
-        alt="FAFO OCTO"
-        width={25}
-        height={25} />Insta</button>
-            </div>
-
-            <div >
-        <button className='text-white'>
-        <Image 
-        src={faceb} 
-        alt="FAFO OCTO"
-        width={25}
-        height={25}/>FB</button>
-        </div>
-
-        <div >
-        <h1 className='text-white'>FAFO</h1>
-        <h1 className='text-white'>Fuck Around Find Out</h1>
-        </div>
-        
-            <ul >
-            <Image 
         src={navLogo} 
         alt="FAFO OCTO"
         width={150}
         height={150} />
-              <Link href="#" className='text-white'>APPERAL</Link>
-                <Link href="#" className='text-white'>CAR WRAPS</Link>
-               <Link href="/contact" className='text-white'>ABOUT</Link>
-               <Link href="/contact" className='text-white'>CONTACT</Link>
+        </a>
+
+        <ul className='flex justify-around '>
+              <Link href="#" className='  text-white rounded  p-2  '>APPERAL</Link>
+                <Link href="#" className='  text-white rounded  p-2 '>CAR WRAPS</Link>
+               <Link href="/contact" className=' text-white rounded  p-2 '>ABOUT</Link>
+               <Link href="/contact" className='  text-white rounded p-2 '>CONTACT</Link>
             </ul>
+          
+        
+
+     <div className='text-center'>
+        <h1 className='text-white text-6xl font-extrabold'>FAFO</h1>
+        <h1 className='text-white text-6xl font-extrabold'>Fuck Around Find Out</h1>
+        </div>
+     
+       
+
+     
+        <button className='rounded-full  '>
+        <Image 
+        src={insta} 
+        alt="FAFO OCTO"
+        width={50}
+        height={50} /></button>
+        
+
+          
+        <button className='rounded-full  '>
+        <Image 
+        src={faceb} 
+        alt="FAFO OCTO"
+        width={50}
+        height={50}/></button>
+        
+        <a>
+        <button className='bg-lime rounded-full '>BOOK CONSULT</button>
+        </a>
+      
           </div>
     </nav>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,23 @@ module.exports = {
     extend: {
       backgroundImage: {
         'my_bg_image' : "url('../assets/HeroImageV2.png')",
-      }
+      },
+      colors:{
+        'white': '#ffffff',
+        'lgtblue': '#00E1FF',
+        'lime': '#33FF7E',
+        'purp': '#6F0F99',
+        'drkblue': '#003440',
+        'black': '#000000'
+      },
+      screens: {
+        'sm': '480px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+        
+      }      
         },
   },
   plugins: [],
